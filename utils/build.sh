63b547482c818e7cdd16954080a763f0bded6e13
Ezzat Chamudi <echamudi@outlook.com>
2020-11-28 20:47:58 +0300

    Update install script

--- utils/build.sh
+++ utils/build.sh
@@ -9,18 +9,18 @@ git checkout 4.3.0
 docker run --rm --workdir /code -v "$PWD":/code "trzeci/emscripten:sdk-tag-1.38.32-64bit" python ./platforms/js/build_js.py build_wasm --build_wasm --build_test --build_flags "-s WASM=1 -s WASM_ASYNC_COMPILATION=0 -s SINGLE_FILE=0 "
 
 # Backup the default build_wasm result
-cp -a ./build_wasm/ ./build_wasm_backup
+# cp -a ./build_wasm/ ./build_wasm_backup
 cd ..
 
 
 
-# SEPARATE WASM
-chmod -R 755 ./opencv/build_wasm/bin
-node seperateBinaryFile.js
+# # SEPARATE WASM
+# chmod -R 755 ./opencv/build_wasm/bin
+# node seperateBinaryFile.js
 
 
 
-# BEAUTIFY JS
-cd opencv/build_wasm/bin
-npx js-beautify opencv.js -r
-cd ../../..
+# # BEAUTIFY JS
+# cd opencv/build_wasm/bin
+# npx js-beautify opencv.js -r
+# cd ../../..
